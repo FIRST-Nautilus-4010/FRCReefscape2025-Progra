9a5b512e0d4777c092007226c86eb47524b270db
DARR-1 <a01665821@tec.mx>
2025-07-17 21:44:40 -0600

    Replace PredefinedPositions enum with PredefinedElevatorPositions for improved clarity and maintainability

--- src/main/java/frc/robot/subsystems/Elevator/PredefinedElevatorPositions.java
+++ src/main/java/frc/robot/subsystems/Elevator/PredefinedElevatorPositions.java
@@ -1,12 +1,12 @@
 package frc.robot.subsystems.Elevator;
 
-public enum PredefinedPositions {
+public enum PredefinedElevatorPositions {
     SOURCE(0, 1), REEF(0, 1), ALGAE(1, 1);
 
     private final double angle;
     private final double height;
 
-    PredefinedPositions(double angle, double height) {
+    PredefinedElevatorPositions(double angle, double height) {
         this.angle = angle;
         this.height = height;
     }
