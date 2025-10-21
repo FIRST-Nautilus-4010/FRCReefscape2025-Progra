49679fc07133555e13aa398a04ba3934f832cb0f
André <151085951+DARR-1@users.noreply.github.com>
2025-10-21 07:49:26 -0600

    Increase autonomous proportional gains
    Raised P_X and P_Y values from 1.5 to 50 in AutonomousConstants to improve autonomous control responsiveness.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,11 +35,11 @@ public final class Constants {
   public static class AutonomousConstants {
     public static final Pose2d initialPose = new Pose2d(); // <-- Initial pose of the robot.
 
-    public static final double P_X = 1.5; // <-- Proportional gain for autonomous control.
+    public static final double P_X = 50; // <-- Proportional gain for autonomous control.
     public static final double I_X = 0; // <-- Integral gain for autonomous control.
     public static final double D_X = 0; // <-- Derivative gain for autonomous control.
 
-    public static final double P_Y = 1.5; // <-- Proportional gain for autonomous control.
+    public static final double P_Y = 50; // <-- Proportional gain for autonomous control.
     public static final double I_Y = 0; // <-- Integral gain for autonomous control.
     public static final double D_Y = 0; // <-- Derivative gain for autonomous control.
 
